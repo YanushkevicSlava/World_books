8b0f65c5e73812e8d8de95af5bcb256ce1c7ca1a
9Nyw1988 <cool.9nyw@gmail.com>
2023-12-04 16:41:40 +0300

    update index

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance
 
 
 def index(request):
-    return HttpResponse('Главная страница сайта "Мир книг"!"')
+    text_head = 'На нашем сайте вы можете получить книги в электронном виде'
+    # Данные о книгах и их количестве
+    books = Book.objects.all()
+    num_books = Book.objects.all().count()
+    # Данные о эземплярах книг в БД
+    num_instances = BookInstance.objects.all().count
+    # Доступные книги (status = 'На складе')
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    # Данные об авторах книг
+    authors = Author.objects.all()
+    num_authors = Author.objects.all().count()
+    # словарь для передачи данных в шаблон index.html
+    context = {
+        'text_head': text_head,
+        'books': books,
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'authors': authors,
+        'num_authors': num_authors,
+    }
+    return render(request, 'catalog/index.html', context)
+
